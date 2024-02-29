133c0c5d5e764b4508467fd5961277ad952f26c1
fausto <couthyapper79@gmail.com>
2024-02-29 16:42:52 -0300

    TypeError: expected str, got dict

--- assistant/views.py
+++ assistant/views.py
@@ -1,25 +1,32 @@
 from dotenv import load_dotenv
-import requests
-from langchain.tools import tool
 from langchain.tools import Tool
-from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage, messages_to_dict
+import json
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import LLMChain
 from langchain_community.chat_message_histories.upstash_redis import UpstashRedisChatMessageHistory
-from langchain.prompts import ChatPromptTemplate,MessagesPlaceholder
-from langchain_openai import OpenAIEmbeddings
+from langchain.prompts import ChatPromptTemplate
 from langchain_community.vectorstores.chroma import Chroma
-from langchain.vectorstores.base import VectorStore
-load_dotenv()
-from typing import List
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
 import csv
+import logging
+import json
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest
+from django.views.decorators.http import require_POST
+from langchain.agents import AgentExecutor, create_json_chat_agent
+load_dotenv()
 
 def read_csv(file_path: str, csv_args: dict = {'delimiter': ','}):
+    """Reads a CSV file and returns its contents as a list of strings.
+
+    Args:
+        file_path (str): The path to the CSV file.
+        csv_args (dict, optional): Additional arguments to be passed to csv.reader. Defaults to {'delimiter': ','}.
+
+    Returns:
+        List[str]: A list containing the contents of the CSV file.
+    """
     data = []
     with open(file_path, 'r') as file:
         csv_reader = csv.reader(file, **csv_args)
@@ -27,45 +34,51 @@ def read_csv(file_path: str, csv_args: dict = {'delimiter': ','}):
             data.append(' '.join(row))
     return data
 
-def chroma_db_tool() -> VectorStore:
-    raw_documents = read_csv('assistantweb/data/products.csv/')
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    chunks = text_splitter.split_documents(raw_documents)
-    embedding_function = OpenAIEmbeddings()
-    db = Chroma.from_documents(chunks, embedding_function)
-    return db
+def chroma_db_tool() -> Chroma:
+    logging.info("Initializing Chroma DB tool...")
+    try:
+        raw_documents = read_csv('assistantweb/data/products.csv')
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        chunks = text_splitter.split_documents(raw_documents)
+        embedding_function = OpenAIEmbeddings()
+        db = Chroma.from_documents(chunks, embedding_function)
+        logging.info(f"Chroma DB initialized successfully with {len(chunks)} chunks.")
+        return db
+    except Exception as e:
+        logging.error(f"Failed to initialize Chroma DB: {e}")
+        raise
 
 
-@tool
 class SearchTool:
     """Search data in ChromaDB using a given query."""
-    name = "chroma_db_search"
-
+    
     def __call__(self, query: str) -> str:
-        """Search data in ChromaDB using a given query."""
+        """Searches data in Chroma vector store using a given query.
+
+        Args:
+            query (str): The query to search for.
+
+        Returns:
+            str: The search result.
+        """
         db = chroma_db_tool()
         docs = db.similarity_search(query)
         return docs[0].page_content if docs else "No results found."
 
+search_tool_instance = SearchTool()  
+
 tools = [
     Tool(
         name="chroma_db_search",
-        func=lambda tool_input: SearchTool().__call__(tool_input),
+        func=search_tool_instance.__call__,
         description="Useful for retrieving data from ChromaDB based on user queries"
     )       
 ]
 
 model = ChatOpenAI(
     model="gpt-3.5-turbo",
-    temperature=0.6
+    temperature=0
 )
-
-prompt = ChatPromptTemplate.from_messages([
-        ("system", "eres belgrano autos, un asistente de una tienda de autos con el objetivo de ayudar a los clientes en sus dudas de una manera amable"),
-        MessagesPlaceholder(variable_name="chat_history"),
-        ("human", "{input}")
-    ])
-
 
 UPSTASH_URL = "https://us1-excited-haddock-41233.upstash.io"
 UPSTASH_TOKEN ="AaERACQgMDliYjdlZWYtYzgzYi00NmJiLTk2MjktNTllNjZjZDBjZWYxMmE5OGRiNGRmZTgxNDc2YjkyMmQ1YzExOWE0NTljNjI="
@@ -75,36 +88,51 @@ history = UpstashRedisChatMessageHistory(
 
 memory = ConversationBufferMemory(
     memory_key="chat_history",
-    return_messages=True,
-    chat_memory=history,
-)
+    input_key="input",
+    return_messages=True
+    )
 
-chain = prompt | model
 
-chain = LLMChain(
+prompt = ChatPromptTemplate.from_messages([
+    (SystemMessage, {"content": "eres un vendedor de un consecionario de autos nuevos, tu labor es informar y ayudar a los clientes en sus compras con la información a disposición, tienes que responder de forma amable y únicamente con la información que tengas a disposición ya sea de la base de datos o de tu memoria. tienes que dar la respuesta en formato json"}),
+    (AIMessage, {"content": "Hola! ¿Cómo puedo ayudarte hoy?"}),
+    (HumanMessage, {"content": "{input}"}),
+])
+json_agent = create_json_chat_agent(
     llm=model,
     prompt=prompt,
-    verbose=True,
-    memory=memory,
-    )
-_call_=lambda input: SearchTool().run(input, tools),
+    tools=tools
+)
 
+agent = AgentExecutor(
+    agent=json_agent,
+    memory=memory,
+    tools=tools,
+)
 
 @csrf_exempt
-def ask(request):
-    try:
-        if request.method == 'POST':
-            question = request.get('question')
-            if question:
-                response = chain(input=question)
-                return handle_response(response)
-            else:
-                return {'error': 'No question provided in the request'}
-    except Exception as e:
-        return {'error': str(e)}
+@require_POST
+def ask(request: HttpRequest):
+    """Handles incoming HTTP POST requests for asking questions.
 
-def handle_response(response):
-    if "error" in response:
-        return JsonResponse({"error": response["error"]}, status=400)
-    else:
-        return JsonResponse({"response": response["response"]})
+    Args:
+        request (HttpRequest): The HTTP request object containing the question.
+
+    Returns:
+        JsonResponse: A JSON response containing the answer to the question.
+    """
+    try:
+        data = json.loads(request.body)
+        # question_text = data.get('question') 
+        if data:
+            # question_content = HumanMessage(content=question_text) 
+            response = agent.invoke(data)
+            print(response)
+            # response_dict =messages_to_dict(response)
+            # response.dumps(response)
+            # return JsonResponse({"response": response})
+        else:
+            return JsonResponse({"error": "Question cannot be blank or null"}, status=400)
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return JsonResponse({'error': str(e)}, status=500)
