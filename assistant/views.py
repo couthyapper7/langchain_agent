9f315d10f8ca83a2c7d9a90f6e86967cb6f765f6
fausto <couthyapper79@gmail.com>
2024-03-11 14:58:32 -0300

    rag assitant

--- assistant/views.py
+++ assistant/views.py
@@ -15,7 +15,15 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpRequest
 from django.views.decorators.http import require_POST
 from langchain.agents import AgentExecutor, create_json_chat_agent
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForToolRun,
+    CallbackManagerForToolRun,
+)
+from langchain.tools.render import render_text_description
+from typing import Optional, Type
+from langchain import hub
 load_dotenv()
+
 
 def read_csv(file_path: str, csv_args: dict = {'delimiter': ','}):
     data = []
@@ -28,7 +36,7 @@ def read_csv(file_path: str, csv_args: dict = {'delimiter': ','}):
 def chroma_db_tool() -> Chroma:
     logging.info("Initializing Chroma DB tool...")
     try:
-        raw_documents = read_csv('path/to/your/csv')
+        raw_documents = read_csv('/home/fausto/assistantweb/data/products.csv')
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         chunks = text_splitter.split_documents(raw_documents)
         embedding_function = OpenAIEmbeddings()
@@ -44,7 +52,7 @@ def chroma_db_tool() -> Chroma:
 class SearchTool(BaseTool):
     name = "chroma_db_search"  
     description = "Useful for retrieving data of cars for the user"  
-    def _run (self, query: str) -> str:
+    def _run (self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None) -> str:
         """Searches data in Chroma vector store using a given query.
 
         Args:
@@ -56,11 +64,9 @@ class SearchTool(BaseTool):
         db = chroma_db_tool()  
         docs = db.similarity_search(query)
         return docs[0].page_content if docs else "No results found."
-    def _arun (self, query: str) -> str:
+    def _arun (self, query: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None) -> str:
         """Use the tool asynchronously."""
         raise NotImplementedError("this tool does not support async")
-
-
 
 tools = [SearchTool()]
 
@@ -79,18 +85,55 @@ memory = ConversationBufferMemory(
     memory_key="chat_history",
     input_key="input",
     return_messages=True
-    )
+)
 
 messages = [
     SystemMessagePromptTemplate(prompt=PromptTemplate(input_variables=[], template='eres un vendedor de un consecionario que debe ser lo mas amable y amigable posible con el cliente e intentar venderle alguno de nuestros autos a toda costa. tu no tienes ningun conocimiento sobre autos asi que debes responder solamente con los datos proporcionados por tu herramienta: chroma_db_search. puedes decorar esa informacion para hacer un mensaje mas comodo de leer y que parezca una interaccion humana (siempre manteniendo la cordialidad) pero no puedes cambiar los valores recibidos por tu herramienta chroma_db_search')),
     MessagesPlaceholder(variable_name='chat_history', optional=True),
-    HumanMessagePromptTemplate(prompt=PromptTemplate(input_variables=['input'], template='{input}')),
-    MessagesPlaceholder(variable_name='agent_scratchpad'),
-    MessagesPlaceholder(variable_name='tools'),
-    MessagesPlaceholder(variable_name='tool_names'),
+    HumanMessagePromptTemplate(prompt=PromptTemplate(input_variables=['input'], template=
+    """TOOLS
+    ------
+    Assistant can ask the user to use tools to look up information that may be helpful in answering the users original question. The tools the human can use are:
+    {tools}
+    RESPONSE FORMAT INSTRUCTIONS
+    ----------------------------
+    When responding to me, please output a response in one of two formats:
+    **Option 1:**
+    Use this if you want the human to use a tool.
+    Markdown code snippet formatted in the following schema:
+    json
+    {{
+        "action": string, \ The action to take. Must be one of {tool_names}
+        "action_input": string \ The input to the action
+    }}
+    Option 2:
+    Use this if you want to respond directly to the human. Markdown code snippet formatted in the following schema:
+    json
+
+    {{
+
+        "action": "Final Answer",
+
+        "action_input": string \ You should put what you want to return to use here
+
+    }}
+    USER'S INPUT
+
+    --------------------
+
+    Here is the user's input (remember to respond with a markdown code snippet of a json blob with a single action, and NOTHING else):
+
+    {input}
+    """
+        )),
+    MessagesPlaceholder(variable_name='agent_scratchpad')
 ]
 
 prompt = ChatPromptTemplate.from_messages(messages)
+
+tools_description = render_text_description(list(tools))
+tool_names = ", ".join([t.name for t in tools])
+
 
 json_agent = create_json_chat_agent(
     llm=model,
@@ -109,6 +152,7 @@ agent = AgentExecutor(
     verbose=True,
     return_intermediate_steps=True,
 )
+
 
 
 @csrf_exempt
